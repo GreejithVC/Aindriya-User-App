54aee045adbf87cb841e9bba8d8b62fa1ab4643f
AthiraGreejith <athirasri1720@gmail.com>
2021-09-10 14:46:49 +0530

    changes

--- superstore/lib/src/models/vendor.dart
+++ superstore/lib/src/models/vendor.dart
@@ -28,7 +28,7 @@ class Vendor {
       rate = jsonMap['rate'];
       distance = jsonMap['distance'];
       logo = jsonMap['logo'];
-      shopTypePreviewImage = jsonMap['logo'];
+      shopTypePreviewImage = jsonMap['previewImage'];
       cover = jsonMap['cover'];
       openStatus = jsonMap['openStatus'];
       longitude = jsonMap['longitude'];
@@ -61,7 +61,7 @@ class Vendor {
     map["rate"] = rate;
     map["distance"] = distance;
     map["logo"] = logo;
-    map["logo"] = shopTypePreviewImage;
+    map["previewImage"] = shopTypePreviewImage;
     map["cover"] = cover;
     map["openStatus"] = openStatus;
     map["longitude"] = longitude;
--- superstore/lib/src/pages/vendor_map.dart
+++ superstore/lib/src/pages/vendor_map.dart
@@ -82,7 +82,7 @@ class _VendorMapWidgetState extends StateMVC<VendorMapWidget> {
               ? CircularLoadingWidget(height: 0)
               : GoogleMap(
                   mapToolbarEnabled: false,
-                  mapType: MapType.normal,
+                  mapType: MapType.hybrid,
                   initialCameraPosition: _con.cameraPosition,
                   markers: Set.from(_con.allMarkers),
                   onMapCreated: (GoogleMapController controller) {
