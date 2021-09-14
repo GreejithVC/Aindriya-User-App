8c1e414a90368a029d3cb00df3b84907bdf67828
AthiraGreejith <athirasri1720@gmail.com>
2021-09-14 11:23:45 +0530

    add fav shop design

--- superstore/lib/src/pages/fav_shops.dart
+++ superstore/lib/src/pages/fav_shops.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:superstore/src/controllers/vendor_controller.dart';
+import 'package:superstore/src/elements/ShopListBoxWidget.dart';
 
-class FavShops extends StatelessWidget {
+class FavShops extends StatefulWidget {
+  final VendorController con;
+  final int shopType;
+  final int focusId;
+  final String previewImage;
+
+  const FavShops({Key key, this.con, this.shopType, this.focusId, this.previewImage}) : super(key: key);
+
+
+  @override
+  _FavShopsState createState() => _FavShopsState();
+}
+
+class _FavShopsState extends State<FavShops> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,9 +43,23 @@ class FavShops extends StatelessWidget {
 
                 ],
               ),
-              
+
             ])
         ),
+      ),
+      body: ListView.separated(
+        scrollDirection: Axis.vertical,
+        itemCount: widget.con.vendorList.length,
+        shrinkWrap: true,
+        primary: false,
+        padding: EdgeInsets.only(top: 16),
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, int index) {
+          return ShopList(choice: widget.con.vendorList[index], shopType: widget.shopType,focusId: widget.focusId,previewImage: widget.previewImage,);
+        },
+        separatorBuilder: (context, index) {
+          return SizedBox(height: 0);
+        },
       ),
 
 
