bfc3f135f8981dd46cd3a9c0daf09fc8fa8bf4af
AthiraGreejith <athirasri1720@gmail.com>
2021-09-14 13:05:12 +0530

    integration of fav shop list

---
+++ superstore/lib/src/controllers/fav_shop_controller.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:superstore/src/models/vendor.dart';
+
+class FavShopController extends ControllerMVC{
+  List<Vendor> favShopList = <Vendor>[];
+
+  // addPackageType(context, id, pageType) {
+  //   if (generalFormKey.currentState.validate()) {
+  //     generalFormKey.currentState.save();
+  //     if (pageType == 'do_add') {
+  //       Overlay.of(context).insert(loader);
+  //       setState(() => shopTypeList.clear());
+  //       FirebaseFirestore.instance.collection('packageDetails').add({
+  //         'name': packageTypeData.packageName,
+  //         'maxProducts': packageTypeData.maxProductSupported,
+  //         'maxCategory': packageTypeData.maxCategorySupported,
+  //         'isFeaturedShop': packageTypeData.isFeaturedShop,
+  //         'monthlyRate': packageTypeData.monthlyRate,
+  //         'yearlyRate': packageTypeData.yearlyRate
+  //       }).catchError((e) {
+  //         print(e);
+  //       }).whenComplete(() {
+  //         Navigator.pop(context);
+  //         listenForPackageTypeList();
+  //         Helper.hideLoader(loader);
+  //       });
+  //       showToast("Update Successfully",
+  //           gravity: Toast.BOTTOM, duration: Toast.LENGTH_SHORT);
+  //     }
+  //   }
+  // }
+
+  // deletePackageType(id) {
+  //   FirebaseFirestore.instance
+  //       .collection('packageDetails')
+  //       .doc(id)
+  //       .delete()
+  //       .then((_) {
+  //     print("success!");
+  //   }).catchError((e) {
+  //     print(e);
+  //   }).whenComplete(() {
+  //     listenForPackageTypeList();
+  //   });
+  //   showToast("Deleted Successfully",
+  //       gravity: Toast.BOTTOM, duration: Toast.LENGTH_SHORT);
+  // }
+  //
+  Future<void> listenForFavShopList() async {
+    favShopList.clear();
+    print("listenForFavShopList///");
+    FirebaseFirestore.instance
+        .collection("favShopList")
+        .get()
+        .then((querySnapshot) {
+      querySnapshot.docs.forEach((result) {
+        print(result);
+        print(result.id);
+        print(result.reference);
+        print(result.data());
+        setState(() => favShopList
+            .add(Vendor.fromJSON(result.data())));
+      });
+    }).catchError((e) {
+      print(e);
+    }).whenComplete(() {
+      print("listenForFavShopList/// completed");
+      print(favShopList.length);
+    });
+  }
+
+
+
+}
--- superstore/lib/src/pages/fav_shops.dart
+++ superstore/lib/src/pages/fav_shops.dart
@@ -1,70 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:superstore/src/controllers/vendor_controller.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:superstore/src/controllers/fav_shop_controller.dart';
+import 'package:superstore/src/elements/EmptyOrdersWidget.dart';
 import 'package:superstore/src/elements/ShopListBoxWidget.dart';
 
 class FavShops extends StatefulWidget {
-  final VendorController con;
-  final int shopType;
-  final int focusId;
-  final String previewImage;
-
-  const FavShops({Key key, this.con, this.shopType, this.focusId, this.previewImage}) : super(key: key);
-
-
   @override
   _FavShopsState createState() => _FavShopsState();
 }
 
-class _FavShopsState extends State<FavShops> {
+class _FavShopsState extends StateMVC<FavShops> {
+  FavShopController _con;
+
+  _FavShopsState() : super(FavShopController()) {
+    _con = controller;
+  }
+
+  @override
+  // ignore: must_call_super
+  void initState() {
+    _con.listenForFavShopList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         titleSpacing: 0,
-        title:Container(
+        title: Container(
             width: double.infinity,
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-
                   Expanded(
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                      Text(
-                        'FAV SHOPS',
-                        style: Theme.of(context).textTheme.subtitle2,
-                        textAlign: TextAlign.left,
-                      ),
-
-                    ]),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'FAV SHOPS',
+                            style: Theme.of(context).textTheme.subtitle2,
+                            textAlign: TextAlign.left,
+                          ),
+                        ]),
                   ),
-
                 ],
               ),
-
-            ])
-        ),
+            ])),
       ),
-      body: ListView.separated(
-        scrollDirection: Axis.vertical,
-        itemCount: widget.con.vendorList.length,
-        shrinkWrap: true,
-        primary: false,
-        padding: EdgeInsets.only(top: 16),
-        physics: NeverScrollableScrollPhysics(),
-        itemBuilder: (context, int index) {
-          return ShopList(choice: widget.con.vendorList[index], shopType: widget.shopType,focusId: widget.focusId,previewImage: widget.previewImage,);
-        },
-        separatorBuilder: (context, index) {
-          return SizedBox(height: 0);
-        },
-      ),
-
-
-
-
+      body: _con.favShopList.isEmpty
+          ? EmptyOrdersWidget()
+          : ListView.separated(
+              scrollDirection: Axis.vertical,
+              itemCount: _con.favShopList.length,
+              shrinkWrap: true,
+              primary: false,
+              padding: EdgeInsets.only(top: 16),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, int index) {
+                return ShopList(
+                  choice: _con.favShopList[index],
+                  shopType: _con.favShopList[index].shopType ?? 0,
+                  focusId: _con.favShopList[index]?.focusType ?? 0,
+                  previewImage: _con.favShopList[index]?.shopTypePreviewImage,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 0);
+              },
+            ),
     );
   }
 }
